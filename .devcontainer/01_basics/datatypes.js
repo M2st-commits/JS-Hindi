908b87416b6fdcf37777b408e25f03b332058c39
M2st-commits <akash.k.mishra03@gmail.com>
2024-11-10 16:45:33 +0000

    container config done

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // tret all JS code as newer version
+// alert(3+3) we are using node js not browser. So this we will not work here only
+
+let name = "Akash"
+let age = 20
+let isLoogedIn = false
+// number => 2 to 53 power
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+console.log(typeof age);
+console.log(name);
+
+// type of null is a object data type and type of undefined is a undefined data type
---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,11 @@
+const accId = 12345
+let accEmail = "akash@gmail.com"
+var accPassword = "12134"
+accCity = "Pune"
+let accState;
+console.table([accId,accEmail,accPassword,accCity,accState])
+
+// Semicolon(;) is not mandatory we can use it or not
+/*
+Do not use var due to block scope problem
+*/

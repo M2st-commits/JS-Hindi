d66c9927a94d8a4337c34f56e59fab77b9a3002d
M2st-commits <akash.k.mishra03@gmail.com>
2024-11-10 17:15:34 +0000

    conversion config done

---
+++ .devcontainer/01_basics/conversions.js
@@ -0,0 +1,18 @@
+let score = "36"   //("36abc") is NaN not a number 
+console.log(typeof(score));
+let valInNumber = Number(score)
+console.log(valInNumber);
+console.log(typeof(valInNumber));
+let isLoggedIn = 1
+console.log(typeof(isLoggedIn));
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof(booleanIsLoggedIn));
+// ""=> false / "aksh" => true
+let someNumber = 30
+console.log(typeof(someNumber));
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+ 
